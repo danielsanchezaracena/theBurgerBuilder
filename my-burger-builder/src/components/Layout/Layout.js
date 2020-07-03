83545a9adb6ed7c9bba1b369e0cf6d446943116e
Daniel Sanchez <daniel.sanchez@evertecinc.com>
2020-07-02 23:14:28 -0400

    Creating a Layout component

---
+++ my-burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+const layout=(props)=>(
+<Aux>
+<div>Toolbar, SideDrawer, backdrop</div>
+<main>
+    {props.children}
+</main>
+</Aux>
+
+);
+
+export default layout;
