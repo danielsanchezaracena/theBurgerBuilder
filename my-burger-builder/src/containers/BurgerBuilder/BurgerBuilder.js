4efe64286581f4a87ed5d6fd7de83729dd4b0260
Daniel Sanchez <daniel.sanchez@evertecinc.com>
2020-07-03 22:24:40 -0400

    Adding a Dynamic Ingredient Component.

---
+++ my-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+    render(){
+        return(
+            <Aux>
+         <div>Burger</div>
+         <div>BuildControls</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder;
